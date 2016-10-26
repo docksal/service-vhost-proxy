d40f37cb6aeb49147f6b7eac3ebb60e4fa62e174
Michael Molchanov <michael.molchanov@ffwagency.com>
2016-10-26 19:32:35 +0300

    Allow to access webui and cadvisor from vhost-proxy nginx.

--- webui/app.py
+++ webui/app.py
@@ -1,14 +1,20 @@
+from werkzeug.wsgi import DispatcherMiddleware
+from werkzeug.serving import run_simple
 from flask import Flask, render_template, redirect, url_for
 from docker import Client
 
 # Debug
 import pprint
 
-app = Flask(__name__)
+webui = Flask(__name__)
+
+app = DispatcherMiddleware(None, {
+    '/webui': webui
+})
 
 cli = Client(base_url='unix://var/run/docker.sock')
 
-@app.route('/')
+@webui.route('/')
 def containers():
     containers = cli.containers(False, True)
     grouped_containers = {
@@ -27,26 +33,29 @@ def containers():
             grouped_containers['other'][container['Id']] = container
     return render_template('containers.html', containers = grouped_containers)
 
-@app.route('/networks')
+@webui.route('/networks')
 def networks():
     return render_template('networks.html', networks = cli.networks())
 
-@app.route('/logs/<container_id>')
+@webui.route('/logs/<container_id>')
 def logs(container_id):
     log = cli.logs(container_id, True, True, False, False, 200)
     return render_template('logs.html', container_id = container_id, log = log.decode('utf-8'))
 
-@app.route('/inspect/container/<container_id>')
+@webui.route('/inspect/container/<container_id>')
 def inspect_container(container_id):
     inspect = cli.inspect_container(container_id)
     return render_template('inspect_container.html', container_id = container_id, inspect = inspect)
 
-@app.route('/remove/container/<container_id>')
+@webui.route('/remove/container/<container_id>')
 def remove_container(container_id):
     cli.remove_container(container_id, False, False, True)
     return redirect(url_for('containers'), code = 302)
 
-@app.route('/inspect/network/<network_id>')
+@webui.route('/inspect/network/<network_id>')
 def inspect_network(network_id):
     inspect = cli.inspect_network(network_id)
     return render_template('inspect_network.html', network_id = network_id, inspect = inspect)
+
+if __name__ == '__main__':
+    run_simple('127.0.0.1', 5000, app, use_reloader=False, use_debugger=False, use_evalex=False)
