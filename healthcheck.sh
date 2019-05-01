a88c994dc2efa1d1e69d9c87006dbbcb82532d3e
Leonid Makarov <leonid.makarov@ffwagency.com>
2019-05-01 11:01:12 -0700

    Optimized healthcheck.sh
    Use "ps" instead of "supervisorctl status" (slow and CPU intensive) to check for running services.

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
-# nginx config is valid
-nginx -t || exit 1
+#set -x  # Print commands
+set -e  # Exit on errors
 
-# supervisor services are running
-if [[ -f /run/supervisord.pid ]]; then
-	supervisorctl status docker-gen | grep RUNNING >/dev/null || exit 1
-	supervisorctl status nginx | grep RUNNING >/dev/null || exit 1
-	supervisorctl status crond | grep RUNNING >/dev/null || exit 1
+# Check nginx config is valid
+echo "Checking nginx configuration..."
+nginx -t
 
-	exit 0
-fi
+# Check supervisor running
+echo "Checking supervisor..."
+# Need to do "|| exit 1" here since "set -e" apparently does not care about tests.
+[[ -f /run/supervisord.pid ]] || exit 1
 
-exit 1
+# Check supervisor controlled services
+# Since "supervisorctl status" is heavy and the healthcheck runs quit often, we use "ps" here
+echo "Checking supervisor services..."
+pslist=$(ps)
+echo ${pslist} | grep docker-gen >/dev/null
+echo ${pslist} | grep "nginx: master process" >/dev/null
+echo ${pslist} | grep "nginx: worker process" >/dev/null
+echo ${pslist} | grep crond >/dev/null
